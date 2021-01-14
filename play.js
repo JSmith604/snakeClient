67ab3a54d1684177729f5a5c04be2cc2bc2c593a
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-14 04:02:10 +0000

    added standard input

--- play.js
+++ play.js
@@ -1,23 +1,55 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
-console.log('You ded cuz you idled!');
-connect();
 
 //play.js 
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+let connection;
+
+
+const handleUserInput = function(key) {
+    // conn.write('Move: up');
+  if (key === 'w') {
+    connection.write('Move: up');
+  } 
+  if (key === 'b') {
+    connection.write('Say: bird');
+  }
+  if (key === 'p') {
+    connection.write('Say: parrot')
+  }
+  if (key === 'c') {
+    connection.write('Say: cat')
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
