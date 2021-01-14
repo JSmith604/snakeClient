82707a7c566cac3a59e56faa6b50d435a3e4111e
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-14 07:34:56 +0000

    added user input

--- play.js
+++ play.js
@@ -1,55 +1,11 @@
-
-
-//play.js 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-};
-
-let connection;
+setupInput();
 
 
-const handleUserInput = function(key) {
-    // conn.write('Move: up');
-  if (key === 'w') {
-    connection.write('Move: up');
-  } 
-  if (key === 'b') {
-    connection.write('Say: bird');
-  }
-  if (key === 'p') {
-    connection.write('Say: parrot')
-  }
-  if (key === 'c') {
-    connection.write('Say: cat')
-  }
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-  if (key === 's') {
-    connection.write('Move: down');
-  }
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
-const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
+
---
+++ userInput.js
@@ -0,0 +1,41 @@
+let connection;
+
+
+const handleUserInput = function(key) {
+    // conn.write('Move: up');
+  if (key === 'w') {
+    connection.write('Move: up');
+  } 
+  if (key === 'b') {
+    connection.write('Say: bird');
+  }
+  if (key === 'p') {
+    connection.write('Say: parrot')
+  }
+  if (key === 'c') {
+    connection.write('Say: cat')
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
